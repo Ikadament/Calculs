d05a0f71f1584f8cb34abf118eae3a60b3f05650
Adam I <IkaCoursInfo@gmail.com>
2023-09-29 19:43:24 +0200

    Ajout d'un commentaire inutile en début de code pour tester les modifs github

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -11,6 +11,8 @@ namespace Calculs
     {
         static void Main(string[] args)
         {
+            //Ligne modification pour tester GitHub
+
             // variables 
             Random rand = new Random(); // outil de génération de nombre aléatoire
             int val1, val2; // mémorisation de nombres aléatoires
